dfebd926114fafa524b039e837c0512dbf124ea0
Josie M. Cantu <josiemcantu@Josies-MacBook-Pro.local>
2021-01-12 15:22:25 -0800

    makes the guesses decrease in the text content

--- app.js
+++ app.js
@@ -10,12 +10,43 @@ const guessResults = document.getElementById('guess-results');
 //const resetButton = document.getElementById('reset-button');
 
 // initialize state
-let randomNumber = 0;
+let randomNumber = Math.ceil(Math.random() * 20);
 let guessesRemaining = 4;
 
 // set event listeners to update state and DOM
 guessButton.addEventListener('click', () => {
-    //guessesRemaining --;
-    //numberGuessed.value = randomNumber;
-    console.log('hiya');
+    guessesRemaining--;
+    guessesSpace.textContent = guessesRemaining;
+    if (guessesRemaining < 1) {
+        guessButton.disabled = true;
+        guessResults.textContent = 'Game Over';
+    }
 });
+
+
+
+
+
+
+
+
+
+
+    //    if (numberGuessed > randomNumber) {
+         
+    //         guessResults.textContent = 'Too High!';
+    //          else {
+    //             ;
+    //         }  
+    //    }  
+
+
+// check if there are guesses left
+                //if not disable input
+            // change the remaining guesses
+
+        // } else if (numberGuessed !== randomNumber) {
+        //     guessResults.textContent = 'Nope!';
+        // } else (numberGuessed === randomNumber) 
+        //     guessResults.textContent = 'Yes! you win!';
+        //});
