09f775c19446fb676d7efd08bccb86b54bf34d24
Josie M. Cantu <josiemcantu@Josies-MacBook-Pro.local>
2021-01-12 13:17:30 -0800

    adds the initialized state to app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // import functions and grab DOM elements
 const numberGuessed = document.getElementById('number-guessed');
 const guessButton = document.getElementById('guess-button');
-const remainingGuesses = document.getElementById('remaining-guesses');
+const guessesSpace = document.getElementById('guesses-space');
 const guessResults = document.getElementById('guess-results');
 const resetButton = document.getElementById('reset-button');
 
 // initialize state
+let randomNumber = 0;
+let guessesRemaining = 4;
 
 // set event listeners to update state and DOM
