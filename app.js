a1e80247edfcd4e686e765d6bc33879f59fb667b
Josie M. Cantu <josiemcantu@Josies-MacBook-Pro.local>
2021-01-12 16:53:24 -0800

    made the guessResults show up in html element

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 // import functions and grab DOM elements
 const numberGuessed = document.getElementById('number-guessed');
-
 const guessButton = document.getElementById('guess-button');
-
 const guessesSpace = document.getElementById('guesses-space');
-
 const guessResults = document.getElementById('guess-results');
 
 //const resetButton = document.getElementById('reset-button');
@@ -15,16 +12,18 @@ let guessesRemaining = 4;
 
 // set event listeners to update state and DOM
 guessButton.addEventListener('click', () => {
+    console.log(randomNumber);
     guessesRemaining--;
     guessesSpace.textContent = guessesRemaining;
-    if (guessesRemaining < 1){
+    if (Number(numberGuessed.value) > randomNumber){
+        guessResults.textContent = 'Too High!';
+    } else if (Number(numberGuessed.value) === randomNumber){
+        guessResults.textContent = 'You win!';
+    } else if (Number(numberGuessed.value) < randomNumber){
+        guessResults.textContent = 'Too low!';
+    } else if (guessesRemaining < 1){
         guessButton.disabled = true;
         guessResults.textContent = 'No more guesses';
-    } else if (numberGuessed > randomNumber){
-        guessResults.textContent = 'Too High!';
-    } else if (numberGuessed === randomNumber){
-        guessResults.textContent = 'You win!';
-    } else if (numberGuessed < randomNumber){
-        guessResults.textContent = 'Too low!';
     }
 });
+// set reset button
