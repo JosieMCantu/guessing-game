d0b694d804f311db136cff1248bc675bc42be1d5
Josie M. Cantu <josiemcantu@Josies-MacBook-Pro.local>
2021-01-16 21:20:15 -0800

    fixed functionality in app.js and utils.js

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 
 // import functions and grab DOM elements
-import { guessNumber } from '/utils.js';
-
+//import { guessNumber } from '/utils.js';
+const guessesSpace = document.getElementById('guesses-space');
+const guessResults = document.getElementById('guess-results');
+const numberGuessed = document.getElementById('number-guessed');
 const guessButton = document.getElementById('guess-button');
 
 let randomNumber = Math.ceil(Math.random() * 20);
 let guessesRemaining = 4;
 
 guessButton.addEventListener('click', () => {
-    guessNumber(randomNumber, guessesRemaining)
+    guessNumber(randomNumber, numberGuessed);
+    guessesSpace.textContent = guessesRemaining;
 });
+
+function guessNumber(randomNumber, numberGuessed){
+    
+    --guessesRemaining;
+
+    if (guessesRemaining < 1){
+        guessButton.disabled = true;
+        guessResults.textContent = 'No more guesses';
+    }else if (Number(numberGuessed.value) > randomNumber){
+        guessResults.textContent = 'Too High!';
+    } else if (Number(numberGuessed.value) === randomNumber){
+        guessResults.textContent = 'You win!';
+    } else if (Number(numberGuessed.value) < randomNumber){
+        guessResults.textContent = 'Too low!';
+    }
+}
